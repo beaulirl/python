b4c02e50fb0086156fb34b8c8a3bf6d234e7a27a
beaulirl <beaulirl@gmail.com>
2017-01-20 14:56:14 +0300

    new changes

--- test_task/task_flights.py
+++ test_task/task_flights.py
@@ -7,30 +7,41 @@ from lxml import html
 
 def get_flights(departure, destination, date_out, date_back=None):
     """Request data about flights"""
-    if check_date(date_out, date_back):
-        with requests.Session() as session:
-            url = 'http://www.flyniki.com/en/start.php'
-            one_way = 1 if not date_back else 0
-            trip_data = {'departure': departure, 'destination': destination, 'outboundDate': date_out,
-                         'returnDate':  date_back, 'adultCount': 1, 'oneway': one_way, 'market': 'US', 'language': 'en'}
-            ajax_trip_data = {
-                '_ajax[templates][]': 'main',
-                '_ajax[requestParams][departure]': departure,
-                '_ajax[requestParams][destination]': destination,
-                '_ajax[requestParams][outboundDate]': date_out,
-                '_ajax[requestParams][returnDate]':  date_back,
-                '_ajax[requestParams][adultCount]': 1,
-                '_ajax[requestParams][oneway]': one_way}
-            try:
-                response1 = session.post(url, data=trip_data, verify=False)
-                response2 = session.post(response1.url, data=ajax_trip_data, verify=False)
-            except requests.exceptions.ConnectTimeout:
-                print "Connection Timeout occurred"
-            except requests.exceptions.ConnectionError:
-                print "Connection Error occurred"
-            except requests.exceptions.HTTPError:
-                print "Http Error occurred"
-            parse_json(response2)
+    if not check_date(date_out, date_back):
+        return 0
+    with requests.Session() as session:
+        url = 'http://www.flyniki.com/en/start.php'
+        if date_back:
+            one_way = 0
+        else:
+            one_way = 1
+            date_back = ''
+        trip_data = {'departure': departure,
+                     'destination': destination,
+                     'outboundDate': date_out,
+                     'returnDate':  date_back,
+                     'adultCount': 1,
+                     'oneway': one_way,
+                     'market': 'US',
+                     'language': 'en'}
+        ajax_trip_data = {
+            '_ajax[templates][]': 'main',
+            '_ajax[requestParams][departure]': departure,
+            '_ajax[requestParams][destination]': destination,
+            '_ajax[requestParams][outboundDate]': date_out,
+            '_ajax[requestParams][returnDate]':  date_back,
+            '_ajax[requestParams][adultCount]': 1,
+            '_ajax[requestParams][oneway]': one_way}
+        try:
+            response1 = session.post(url, data=trip_data, verify=False)
+            response2 = session.post(response1.url, data=ajax_trip_data, verify=False)
+        except requests.exceptions.ConnectTimeout:
+            print "Connection Timeout occurred"
+        except requests.exceptions.ConnectionError:
+            print "Connection Error occurred"
+        except requests.exceptions.HTTPError:
+            print "Http Error occurred"
+        return response2
 
 
 def check_date(date_out, date_back=None):
@@ -39,11 +50,9 @@ def check_date(date_out, date_back=None):
     if not date_back:
         datetime_return = datetime.datetime.strptime('9999-12-31', '%Y-%m-%d').date()
     else:
-        date_return = date_back[0]
-        datetime_return = datetime.datetime.strptime(date_return, '%Y-%m-%d').date()
+        datetime_return = datetime.datetime.strptime(date_back, '%Y-%m-%d').date()
     datetime_out = datetime.datetime.strptime(date_out, '%Y-%m-%d').date()
     if datetime_out < today or datetime_return < today or datetime_return < datetime_out:
-        print "Please, check your date"
         return 0
     else:
         return 1
@@ -54,14 +63,15 @@ def parse_json(response):
     flight_info_json = json.loads(response.text)
     flight_data = flight_info_json['templates']['main'].replace("\\", "")
     root = html.fromstring(flight_data)
-    flight_info_xml = root.xpath('//span[@title]/@title')
-    flights_list = []
+    flight_info_xml = root.xpath('//span/@title')
+    flight_list = set(flight_info_xml)
     print flight_info_xml[0], "Pounds"
     for k in range(len(flight_info_xml)):
-        if k != len(flight_info_xml)-1 and flight_info_xml[k][36:50] != flight_info_xml[k + 1][36:50] and \
-                        flight_info_xml[k+1] not in flights_list:
-            flights_list.append(flight_info_xml[k+1])
-            print flight_info_xml[k+1], "Pounds"
+        if k != len(flight_info_xml)-1 and flight_info_xml[k][36:50] != flight_info_xml[k + 1][36:50] \
+                and flight_info_xml[k] in flight_list:
+            print flight_info_xml[k+1]
 
 if __name__ == '__main__':
-    print get_flights('VCE', 'STR', '2017-02-13')
+    result_json = get_flights('VCE', 'STR', '2017-02-13', '2017-02-14')
+    parse_json(result_json)
+

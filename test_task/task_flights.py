b3676ccc2abc4e19199d12b3428213f6750c6d3e
beaulirl <beaulirl@gmail.com>
2017-01-27 13:44:33 +0300

    parse changes

--- test_task/task_flights.py
+++ test_task/task_flights.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import requests
+import sys
 
 from lxml import html
 
@@ -14,6 +15,9 @@ def get_flights(departure, destination, date_out, date_back=None):
         else:
             one_way = 1
             date_back = ''
+        response = session.get(url)
+        root = html.fromstring(response.text)
+        market_value = root.xpath('//select[contains(@id, "market")]//option/text()')
         trip_data = {
             'departure': departure,
             'destination': destination,
@@ -21,7 +25,7 @@ def get_flights(departure, destination, date_out, date_back=None):
             'returnDate':  date_back,
             'adultCount': 1,
             'oneway': one_way,
-            'market': 'US',
+            'market': market_value[0],
             'language': 'en'}
         ajax_trip_data = {
             '_ajax[templates][]': 'main',
@@ -32,7 +36,7 @@ def get_flights(departure, destination, date_out, date_back=None):
             '_ajax[requestParams][adultCount]': 1,
             '_ajax[requestParams][oneway]': one_way}
         try:
-            response1 = session.post(url, data=trip_data, verify=False)
+            response1 = session.post(response.url, data=trip_data, verify=False)
             response2 = session.post(response1.url, data=ajax_trip_data, verify=False)
         except requests.exceptions.ConnectTimeout:
             print "Connection Timeout occurred"
@@ -40,7 +44,8 @@ def get_flights(departure, destination, date_out, date_back=None):
             print "Connection Error occurred"
         except requests.exceptions.HTTPError:
             print "Http Error occurred"
-        return response2
+        else:
+            return response2
 
 
 def check_date(date_out, date_back=None):
@@ -63,17 +68,35 @@ def parse_json(response):
     flight_info_json = json.loads(response.text)
     flight_data = flight_info_json['templates']['main'].replace("\\", "")
     root = html.fromstring(flight_data)
-    flight_info_xml = root.xpath('//div[contains(@class, "lowest")]//span/@title')
-    for m in flight_info_xml:
-        print m, "Pounds"
+    flight_info_xml = root.xpath('//div[contains(@class, "lowest")]//span')
+    departure_list = []
+    destination_list = []
+    if len(sys.argv) != 4:
+        for m in flight_info_xml:
+            if m.get('title')[0:3] == sys.argv[1]:
+                temp_list1 = [m.get('title'), m.text]
+                departure_list.append(temp_list1)
+            elif m.get('title')[0:3] == sys.argv[2]:
+                temp_list2 = [m.get('title'), m.text]
+                destination_list.append(temp_list2)
+        for n in range(len(departure_list)):
+            for m in range(len(destination_list)):
+                print departure_list[n][0], destination_list[m][0],  "Total cost:", \
+                    float(departure_list[n][1])+float(destination_list[m][1])
+    else:
+        for d in flight_info_xml:
+            print d.get('title')
 
 
 def scrape():
     """Start all functions"""
-    departure = 'VCE'
-    destination = 'STR'
-    date_out = '2017-01-21'
-    date_back = '2017-01-22'
+    departure = sys.argv[1]
+    destination = sys.argv[2]
+    date_out = sys.argv[3]
+    if len(sys.argv) == 4:
+        date_back = None
+    else:
+        date_back = sys.argv[4]
     if check_date(date_out, date_back):
         result_json = get_flights(departure, destination, date_out, date_back)
         if result_json:
@@ -81,5 +104,4 @@ def scrape():
 
 if __name__ == '__main__':
     scrape()
-
 
